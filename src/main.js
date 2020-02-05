416871b56159331d39960a549e095c67e707397e
elliot jordan and Rachel Schieferstein <rachel@>
2020-02-04 16:30:53 -0800

    successful html and css structuring

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+// import { Player } from './player';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+$(document).ready(function() {
+
+});
---
+++ src/sudoku.js
@@ -0,0 +1,7 @@
+
+export function Board() {
+  this.numbers = [1,2,3,4,5,6,7,8,9];
+  
+
+
+}
